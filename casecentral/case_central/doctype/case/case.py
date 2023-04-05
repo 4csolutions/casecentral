8077cb443cdf36a6c355d378e6b5c9a068391e87
Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>
2023-04-05 16:38:29 +0530

    feat: Calendar view for Case based on next hearing date

--- casecentral/case_central/doctype/case/case.py
+++ casecentral/case_central/doctype/case/case.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2023, 4C Solutions and contributors
 # For license information, please see license.txt
 
+import json
+
 import frappe
 from frappe.model.document import Document
 
@@ -11,3 +13,32 @@ class Case(Document):
 		if case_history:
 			frappe.db.set_value(self.doctype, self.name, 'next_hearing_date', case_history[0][0])
 			self.reload()
+
+@frappe.whitelist()
+def get_events(start, end, filters=None):
+	"""Returns events for Gantt / Calendar view rendering.
+
+	:param start: Start date-time.
+	:param end: End date-time.
+	:param filters: Filters (JSON).
+	"""
+	from frappe.desk.calendar import get_event_conditions
+
+	conditions = get_event_conditions("Case", filters)
+
+	data = frappe.db.sql(
+		"""
+		select
+			name, concat(name, CHAR(13), registration_number) as title, status, next_hearing_date
+		from
+			`tabCase`
+		where status="InProgress"
+			and (next_hearing_date between %(start)s and %(end)s)
+			{conditions}
+		""".format(
+			conditions=conditions
+		),
+		{"start": start, "end": end},
+		as_dict=True
+	)
+	return data
