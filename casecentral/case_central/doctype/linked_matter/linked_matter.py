e52a12ea5bb50ff7e8d26c6e4ebc7222bd044a9b
Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>
2023-05-20 17:01:58 +0530

    feat: Add linked matters to matter.

---
+++ casecentral/case_central/doctype/linked_matter/linked_matter.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, 4C Solutions and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class LinkedMatter(Document):
+	pass
