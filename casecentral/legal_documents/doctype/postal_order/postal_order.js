222ada50b0db41c05381ac94600236e2f25566b1
Syed Mujeer Hashmi <mujeerhashmi@4csolutions.in>
2023-11-27 20:30:59 +0530

    fix: Auto set postal order status on posting date

--- casecentral/legal_documents/doctype/postal_order/postal_order.js
+++ casecentral/legal_documents/doctype/postal_order/postal_order.js
@@ -2,7 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Postal Order', {
-	// refresh: function(frm) {
-
-	// }
+	posting_date: function(frm) {
+		if(frm.doc.posting_date && frm.doc.status == "") {
+			frm.set_value("status", "Posted")
+		} else if(!frm.doc.posting_date) {
+			frm.set_value("status", "")
+		}
+	}
 });
